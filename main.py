31d7793ae043aa3be001e0ab1e359bc3278e7797
Adedoyin Adedara <adedoyin.adedara@gmail.com>
2026-03-28 23:12:55 +0100

    feat: add multi-criteria search by author and category

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = FastAPI()
 # the hard coded database for the project
 BOOKS = [
     {"title": "Clean Code", "author": "Robert C. Martin", "category": "Programming"},
-    {"title": "The Hobbit", "author": "J.R.R. Tolkien", "category": "fantasy"},
+    {"title": "The Hobbit", "author": "James Clear", "category": "fantasy"},
     {"title": "Atomic Habits", "author": "James Clear", "category": "fantasy"},
     {"title": "The Great Gatsby", "author": "F. Scott Fitzgerald", "category": "Classic"},
     {"title": "Deep Learning", "author": "Ian Goodfellow", "category": "AI & Tech"},
@@ -39,10 +39,10 @@ async def read_book(book_title: str):
     return {"Error": "Book not found"}
 
 """
+
 you can perform search on the database based on some argument
 1. you can request a single information just as the previous code above 
 2. you can search for a list of information based on similarity in the database
-
 """
 @app.get("/books/") #this is to tell the database you want to search by list
 async def search_book(searched: str):
@@ -52,5 +52,15 @@ async def search_book(searched: str):
             search_term.append(book)
     return search_term
 
-#
+"""
+we can as well add two conditions for our search 
+"""
+@app.get("/books/{author}/")
+async def search_book_with_others(author: str, category: str):
+    searched = [] #this can return more than one detail if possible
+    for book in BOOKS:
+        if book.get("author").casefold() == author.casefold() and \
+                book.get("category").casefold() == category.casefold():#adding \ makes the line of code continued without breaking or
+            searched.append(book)
+    return searched
 
