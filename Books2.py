e6061693e3fcd5ca8210f999b472ecea4c680068
Adedoyin Adedara <adedoyin.adedara@gmail.com>
2026-04-02 17:33:14 +0100

    feat: add published_date field and date-based filtering endpoint

--- Books2.py
+++ Books2.py
@@ -2,7 +2,7 @@
 This is a new project for the continuation of the book project we are working on.
 
 """
-from dataclasses import Field
+from dataclasses import Field, field
 
 from fastapi import FastAPI
 from pydantic import BaseModel, Field
@@ -15,13 +15,15 @@ class Book:
     author: str
     description: str
     rating: int
+    published_date: int
 
-    def __init__(self, id: int, title: str, author: str, description: str, rating: int) -> None:
+    def __init__(self, id: int, title: str, author: str, description: str, rating: int, published_date: int ) -> None:
         self.id = id
         self.title = title
         self.author = author
         self.description = description
         self.rating = rating
+        self.published_date = published_date
 
 # I will create a new class for the book request inheriting the BaseModel from the pydantic
 # I will implement validation for every of my data
@@ -32,6 +34,7 @@ class BookRequest(BaseModel):
     author: str = Field(min_length=3, max_length=25)
     description: str = Field(min_length=1, max_length=30)
     rating: int = Field(gt=-1, lt=6)
+    published_date: int = Field(gt=0, lt=2027)
 
 #     we can change the default display of the value on the swagger schema
     model_config = {
@@ -40,7 +43,8 @@ class BookRequest(BaseModel):
                 "title":"A default Book",
                 "author":"Benson Adedara",
                 "description":"This is just a sample book for the schema",
-                "rating":5
+                "rating":5,
+                "published_date":2002
             }
         }
     }
@@ -51,11 +55,11 @@ app = FastAPI()
 
 # let's create a list of books
 BOOKS = [
-    Book(id=1, title="Read Well", author="George Best", description="A very good book", rating=5),
-    Book(id=2, title="Just a life", author="Ben Thompson", description="An awesome book", rating=4),
-    Book(id=3, title="Early Programmer", author="Benson Adedara", description="Teaches greatness ", rating=5),
-    Book(id=4, title="give a title", author="no one", description="just a description", rating=3),
-    Book(id=5, title="give a title", author="no one", description="Just a description ", rating=4),
+    Book(id=1, title="Read Well", author="George Best", description="A very good book", rating=5, published_date=2010),
+    Book(id=2, title="Just a life", author="Ben Thompson", description="An awesome book", rating=4, published_date=2010),
+    Book(id=3, title="Early Programmer", author="Benson Adedara", description="Teaches greatness ", rating=5, published_date=2015),
+    Book(id=4, title="give a title", author="no one", description="just a description", rating=3, published_date=2020),
+    Book(id=5, title="give a title", author="no one", description="Just a description ", rating=4, published_date=2026),
 ]
 
 
@@ -115,3 +119,26 @@ async def new_book(book: BookRequest):
 def book_id(book: Book):
      book.id =1 if len(BOOKS) == 0 else BOOKS[-1].id +1
      return book
+
+"""
+Assignment
+
+Here is your opportunity to keep learning!
+
+Add a new field to Book and BookRequest called published_date: int (for example,
+published_date: int = 2012). So, this book as published on the year of 2012.
+
+Enhance each Book to now have a published_date
+
+Then create a new GET Request method to filter by published_date
+"""
+
+# Having fix the necessary published_date everywhere,
+# let's create an endpoint for it
+@app.get("/books/published_date/")
+async def read_all_books_published_date(date : int):
+    published_book_by_date = []
+    for book in BOOKS:
+        if book.published_date == date:
+            published_book_by_date.append(book)
+    return published_book_by_date
